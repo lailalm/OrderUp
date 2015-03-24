0f89ac92556c86cc00ce2482b5623c6c94a61fb8
lailalm <mauhibah@yahoo.com>
2015-03-25 00:57:58 +0700

    initialize the project

---
+++ app/Http/routes.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+/*
+|-------------------------------------------------------------------------
+| Dummy Routes
+|-------------------------------------------------------------------------
+*/
+
+Route::get('/', 'WelcomeController@index');
+
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
+
+/*
+|-------------------------------------------------------------------------
+| Main Website Customer Routes
+|-------------------------------------------------------------------------
+*/
+
+
+/*
+|-------------------------------------------------------------------------
+| Admin Area Routes
+|-------------------------------------------------------------------------
+*/
