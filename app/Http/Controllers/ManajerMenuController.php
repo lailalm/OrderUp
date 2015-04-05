2fdcdc954728439733c7e43a3102f3f38ce5753f
lailalm <mauhibah@yahoo.com>
2015-04-05 10:56:56 +0700

    login UI Karyawan

--- app/Http/Controllers/ManajerMenuController.php
+++ app/Http/Controllers/ManajerMenuController.php
@@ -28,10 +28,10 @@ class ManajerMenuController extends Controller {
 	 *
 	 * @return void
 	 */
-	public function __construct()
-	{
-		$this->middleware('auth');
-	}
+	// public function __construct()
+	// {
+	// 	$this->middleware('auth');
+	// }
 
 	/**
 	 * Show the application dashboard to the user.
