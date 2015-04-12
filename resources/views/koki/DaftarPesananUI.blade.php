b3b380880e88c26498b8565767f00093e3ea61e5
lailalm <mauhibah@yahoo.com>
2015-04-12 20:17:43 +0700

    daftar pesanan

--- resources/views/koki/DaftarPesananUI.blade.php
+++ resources/views/koki/DaftarPesananUI.blade.php
@@ -14,57 +14,58 @@
 					<p class="style-font-meja">Meja {!! App\Meja::find($pemesanan->id_meja)->nomormeja !!}</p>
 				</div>
 			</div>
-		<div class="col-xs-12 row baris-pesan">
-			<div class="col-sm-3 col-xs-4 konten-status-menu">
-				{!! App\Menu::find($pemesanan->id_menu)->name !!}
-			</div>
-			<div class="col-sm-2 col-xs-4 konten-status-menu text-center">
-				{{$pemesanan->jumlah}} Porsi
-			</div>
-			<div class="col-sm-3 col-xs-4 konten-status-menu text-center">
-				{{$pemesanan->keterangan}}
-			</div>
-			<div class="clearfix visible-xs-block"></div>
-		
-			<div class="col-sm-4 col-xs-12 text-center">
-				<div id = "btn-status-pesan" class="btn-group" data-toggle="buttons">
-
-					<label id="btn-wait-{{$pemesanan->id_pemesanan}}" class="btn">
-						<input type="radio" name="options" id="wait"> Waiting
-				  	</label>
-				  	<label id="btn-process-{{$pemesanan->id_pemesanan}}" class="btn">
-						<input type="radio" name="options" id="process"> On Process
-				  	</label>
-				  	<label id="btn-done-{{$pemesanan->id_pemesanan}}" class="btn">
-						<input type="radio" name="options" id="done"> Done
-				  	</label>
-
-
-				  	<script>
-						$('#btn-wait-{{$pemesanan->id_pemesanan}}').click(function(){  
-						  $(this).css('background-color', '#bb2828');
-						  $('#btn-process-{{$pemesanan->id_pemesanan}}').css('background-color', '#9f9f9f');
-						  $('#btn-done-{{$pemesanan->id_pemesanan}}').css('background-color', '#9f9f9f');
-						});
-						
-						$('#btn-process-{{$pemesanan->id_pemesanan}}').click(function(){  
-						  $('#btn-wait-{{$pemesanan->id_pemesanan}}').css('background-color', '#9f9f9f');
-						  $(this).css('background-color', '#fce809');
-						  $('#btn-done-{{$pemesanan->id_pemesanan}}').css('background-color', '#9f9f9f');
-						});
-						
-						$('#btn-done-{{$pemesanan->id_pemesanan}}').click(function(){  
-						  $('#btn-wait-{{$pemesanan->id_pemesanan}}').css('background-color', '#9f9f9f');
-						  $('#btn-process-{{$pemesanan->id_pemesanan}}').css('background-color', '#9f9f9f');
-						  $(this).css('background-color', 'green');
-						});
-					</script>
+			<div class="col-xs-12 row baris-pesan">
+				<div class="col-sm-3 col-xs-4 konten-status-menu">
+					{!! App\Menu::find($pemesanan->id_menu)->name !!}
+				</div>
+				<div class="col-sm-2 col-xs-4 konten-status-menu text-center">
+					{{$pemesanan->jumlah}} Porsi
+				</div>
+				<div class="col-sm-3 col-xs-4 konten-status-menu text-center">
+					{{$pemesanan->keterangan}}
 				</div>
 				<div class="clearfix visible-xs-block"></div>
+			
+				<div class="col-sm-4 col-xs-12 text-center">
+					<div id = "btn-status-pesan" class="btn-group">
+						@if( $pemesanan->status == 'Queued')
+							<a href="{{ url('/changestatus/waiting/'. $pemesanan->id_pemesanan) }}" class="btn btn-primary">
+								Waiting
+						  	</a>
+						  	<a href="{{ url('/changestatus/process/'. $pemesanan->id_pemesanan) }}" class="btn abu">
+								On Process
+						  	</a>
+						  	<a href="{{ url('/changestatus/done/'. $pemesanan->id_pemesanan) }}" class="btn abu">
+								Done
+						  	</a>
+					  	@elseif ($pemesanan->status == 'On Process')
+						  	<a href="{{ url('/changestatus/waiting/'. $pemesanan->id_pemesanan) }}" class="btn abu">
+								Waiting
+						  	</a>
+						  	<a href="{{ url('/changestatus/process/'. $pemesanan->id_pemesanan) }}" class="btn btn-warning">
+								On Process
+						  	</a>
+						  	<a href="{{ url('/changestatus/done/'. $pemesanan->id_pemesanan) }}" class="btn abu">
+								Done
+						  	</a>
+						@else
+							<a href="{{ url('/changestatus/waiting/'. $pemesanan->id_pemesanan) }}" class="btn abu">
+								Waiting
+						  	</a>
+						  	<a href="{{ url('/changestatus/process/'. $pemesanan->id_pemesanan) }}" class="btn abu">
+								On Process
+						  	</a>
+						  	<a href="{{ url('/changestatus/done/'. $pemesanan->id_pemesanan) }}" class="btn btn-success">
+								Done
+						  	</a>
+					  	@endif
+					  	
+					</div>
+					<div class="clearfix visible-xs-block"></div>
+				</div>
+				<div class="clearfix visible-xs-block"></div>
+				<div class="clearfix visible-sm-block"></div>
 			</div>
-			<div class="clearfix visible-xs-block"></div>
-			<div class="clearfix visible-sm-block"></div>
-		</div>
 		@endforeach
 	</div>
 </div>
